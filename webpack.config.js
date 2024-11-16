469ff0612a7d710c2121d1bb88bcfd0b7693adbe
rinriko <rinrikosung@hotmail.com>
2024-11-16 10:37:57 -0600

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const glob = require("glob");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
-  },
-  output: {
-    filename: "build/static/js/bundle.min.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  }
+    mode: "production",
+    entry: {
+        "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+    },
+    output: {
+        filename: "build/static/js/bundle.min.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
 };
